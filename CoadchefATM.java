5a8b6203aded00a78fbb7ce290f8f4ad1c16e282
Elangosundaramurthy <133782574+Elangosundaramurthy@users.noreply.github.com>
2023-07-23 00:12:04 +0530

    Program

---
+++ CoadchefATM.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class CoadchefATM {
+    public static void main (String[] args)
+      {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        double y=sc.nextDouble();
+        if((x+0.50<y)&&(x%5==0))
+    {
+        y=y-(x+0.50);
+        System.out.printf("%.2f",y);
+    }
+    else
+    {
+        System.out.printf("%.2f",y);
+    }
+     }
+}
